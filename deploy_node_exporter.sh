33687f753ec7cd4c2fe4e81664a701ca3353840a
Aruna Lakmal <arunarandunu@gmail.com>
2020-06-27 08:43:18 +0530

    Adding node exporters

---
+++ deploy_node_exporter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Setting up the user, permissions
+sudo useradd --no-create-home --shell /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v0.17.0/node_exporter-0.17.0.linux-amd64.tar.gz
+tar -xvf node_exporter-0.17.0.linux-amd64.tar.gz
+cd node_exporter-0.17.0.linux-amd64/
+sudo mv node_exporter /usr/local/bin/
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+#Setting up unit files
+sudo cp Node_Exporter/node_exporter.service /etc/systemd/system/node_exporter.service
+
+#Starting Services
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter
