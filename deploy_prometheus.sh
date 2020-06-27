f73de2328bbba0b2ab677456666783e8f68013fa
Aruna Lakmal <arunarandunu@gmail.com>
2020-06-27 08:24:27 +0530

    Update deploy_prometheus.sh

--- deploy_prometheus.sh
+++ deploy_prometheus.sh
@@ -5,21 +5,19 @@ sudo useradd --no-create-home --shell /bin/false prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
 sudo chown prometheus:prometheus /var/lib/prometheus
+sudo mv Prometheus/prometheus.yml /etc/prometheus
+sudo cp Prometheus/prometheus.service /etc/systemd/system/prometheus.service
 
 #Download Prometheus and setting up
 wget https://github.com/prometheus/prometheus/releases/download/v2.7.1/prometheus-2.7.1.linux-amd64.tar.gz
 tar -xvf prometheus-2.7.1.linux-amd64.tar.gz
 cd prometheus-2.7.1.linux-amd64
 sudo mv console* /etc/prometheus
-sudo mv prometheus.yml /etc/prometheus
 sudo chown -R prometheus:prometheus /etc/prometheus
 sudo mv prometheus /usr/local/bin/
 sudo mv promtool /usr/local/bin/
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
-
-#Systemd Unitfiles
-sudo cp prometheus.service /etc/systemd/system/prometheus.service
 
 #Setting up the service and starting
 sudo systemctl daemon-reload
